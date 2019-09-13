793cffd507345631de34e682009ea2b945ac7cdd
dilietol <dilietol@gmail.com>
2019-09-13 18:59:35 +0200

    Send class base version; test initial version

--- sqsd/sqsd.py
+++ sqsd/sqsd.py
@@ -1,36 +1,49 @@
 import requests
 import logging
+import datetime
 
 default_url="http://localhost:5000/employees"
-default_headers={'Content-type': 'application/json'}
+default_url2="http://localhost:5001/employees"
+queue_name="nomecodedacambiare"
+default_headers={'User-Agent': 'aws-sqsd',
+                 'X-Aws-Sqsd-Queue': queue_name,
+                 'Content-Type': 'application/json'}
 logging.basicConfig(format='%(asctime)s %(process)d:%(levelname)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
 class Sender():
+
+    def send(self, datas, msgID, url = default_url2, headers = default_headers):
+        self.addMsgHeaders(headers, msgID)
+        r=requests.post(url, json=datas, headers=headers)
+        logging.debug("Post sent\n Json: %s\n headers:%s ", datas, headers)
+        return r.status_code
+
+    def addMsgHeaders(self, headers, msgID):
+        headers.update({'X-Aws-Sqsd-Msgid': str(msgID),
+                        'X-Aws-Sqsd-First-Received-At': datetime.datetime.utcnow().isoformat(),
+                        'X-Aws-Sqsd-Receive-Count': str(0)})
+
+
+class TestSender():
     def generateData(self, index=1):
         return {"index": index , "message": "lorem Ipsum"}
 
-
-    def send(self, datas, url = default_url, headers = default_headers):
-
-        requests.post(url, json=datas, headers=headers)
-        logging.debug("Post sent\n Json: %s\n headers:%s ", datas, headers)
-
-
-    def sendMultiple(self, index=5):
+    def testSenderMultiple(self, index=2):
+        sender = Sender()
         for i in range(index):
-            self.send(self.generateData(i))
+            status = sender.send(self.generateData(i), i)
+            logging.debug("Response Status Code: "+ str(status))
 
-
-app = Sender()
+app = TestSender()
 
 if __name__ == "__main__":
     from sys import argv
 
     logging.info("Started")
     if len(argv) == 2:
-        app.sendMultiple(index=int(argv[1]))
+        app.testSenderMultiple(index=int(argv[1]))
     else:
-        app.sendMultiple()
+        app.testSenderMultiple()
     logging.info("Closed")
